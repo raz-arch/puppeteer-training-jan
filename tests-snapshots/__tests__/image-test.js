eb87a03e4e27bae9c7b9b542b0698c60b5a60a0c
raz-arch <rahulraj@qburst.com>
2020-01-29 18:13:48 +0530

    mocha framework  update

--- tests-snapshots/__tests__/image-test.js
+++ tests-snapshots/__tests__/image-test.js
@@ -6,26 +6,30 @@ expect.extend({ toMatchImageSnapshot })
 describe('my first snapshot test', () => {
 	let browser
 	let page
-	beforeAll(async function(){
-        browser = await puppeteer.launch({
-            headless: false,
+	beforeAll(async function() {
+		browser = await puppeteer.launch({
+			headless: false,
+			args: ['--window-size=1366,768'],
+		})
+		page = await browser.newPage()
+	})
+	afterAll(async function() {
+		await browser.close()
+	})
+	test('homepage snapshot', async () => {
+		await page.setViewport({
+			width: 1366,
+			height: 768,
+		})
+		await page.goto('http://zero.webappsecurity.com/index.html')
+		const image = await page.screenshot()
+		expect(image).toMatchImageSnapshot()
+	}, 30000)
 
-        })
-        page = await browser.newPage()
-    })
-    afterAll(async function(){
-        await browser.close()
-    })
-    test('homepage snapshot', async() => {
-        await page.goto('http://example.com')
-        const image =  await page.screenshot()
-        expect(image).toMatchImageSnapshot()
-    },30000)
-
-    test('single element snapshot', async() => {
-        await page.goto('http://example.com')
-        const h1 = await page.waitForSelector('h1');
-        const image =  await h1.screenshot()
-        expect(image).toMatchImageSnapshot()
-    })
+	test('single element snapshot', async () => {
+		await page.goto('http://zero.webappsecurity.com/index.html')
+		const navbar = await page.waitForSelector('.navbar')
+		const image = await navbar.screenshot()
+		expect(image).toMatchImageSnapshot()
+	}, 30000)
 })
--- tests/examples.js
+++ tests/examples.js
@@ -4,13 +4,8 @@ const expect = require('chai').expect
 
 //helper functions
 const config = require('../lib/config')
-const click = require('../lib/helper').click
-const typeText = require('../lib/helper').typeText
-const loadUrl = require('../lib/helper').loadUrl
-const waitForText = require('../lib/helper').waitForText
-const shouldExist = require('../lib/helper').shouldExist
-const pressKey = require('../lib/helper').pressKey
-const getCount = require('../lib/helper').getCount
+const helper = require('../lib/helper')
+
 //utility functions
 const utils = require('../lib/utils')
 
@@ -22,7 +17,7 @@ const feedbackPage = require('../page-objects/feedback-page')
 const feedbackResultsPage = require('../page-objects/feedbackResult-page')
 const forgetPasswordPage = require('../page-objects/forgotPassword-page')
 
-describe('puppeteer tests', () => {
+describe('puppeteer-mocha tests', () => {
 	let browser
 	let page
 
@@ -47,44 +42,44 @@ describe('puppeteer tests', () => {
 	})
 	describe('Login Test', () => {
 		it('should navigate to homepage', async () => {
-			await loadUrl(page, config.baseUrl)
-			await shouldExist(page, homePage.BANKING_FEATURES)
+			await helper.loadUrl(page, config.baseUrl)
+			await helper.shouldExist(page, homePage.BANKING_FEATURES)
 		})
 
 		it('should click on signin button', async () => {
-			await click(page, homePage.SIGN_IN_BUTTON)
-			await shouldExist(page, loginPage.LOGIN_FORM)
+			await helper.click(page, homePage.SIGN_IN_BUTTON)
+			await helper.shouldExist(page, loginPage.LOGIN_FORM)
 		})
 
 		it('should submit login form', async () => {
-			await typeText(page, loginPage.USER_NAME, utils.generateID(9))
-			await typeText(page, loginPage.USER_PASSWORD, utils.generateNumbers())
-			await click(page, loginPage.SUBMIT_BUTTON)
+			await helper.typeText(page, loginPage.USER_NAME, utils.generateID(9))
+			await helper.typeText(page, loginPage.USER_PASSWORD, utils.generateNumbers())
+			await helper.click(page, loginPage.SUBMIT_BUTTON)
 		})
 
 		it('should get error message', async () => {
-			await waitForText(page, 'Login and/or password are wrong', 'body')
-			await shouldExist(page, loginPage.LOGIN_FORM)
+			await helper.waitForText(page, 'Login and/or password are wrong', 'body')
+			await helper.shouldExist(page, loginPage.LOGIN_FORM)
 		})
 	})
 	describe('Search Test', () => {
 		it('should navigate to homepage', async () => {
-			await loadUrl(page, config.baseUrl)
-			await shouldExist(page, homePage.BANKING_FEATURES)
+			await helper.loadUrl(page, config.baseUrl)
+			await helper.shouldExist(page, homePage.BANKING_FEATURES)
 		})
 
 		it('should submit search phrase', async () => {
-			await typeText(page, homePage.SEARCH_BAR, 'hello world')
-			await pressKey(page, 'Enter')
+			await helper.typeText(page, homePage.SEARCH_BAR, 'hello world')
+			await helper.pressKey(page, 'Enter')
 		})
 
 		it('should display search results', async () => {
-			await waitForText(
+			await helper.waitForText(
 				page,
 				'Search Results',
 				searchResultsPage.SEARCH_RESULTS_TITLE
 			)
-			await waitForText(
+			await helper.waitForText(
 				page,
 				'No results were found for the query',
 				searchResultsPage.SEARCH_RESULTS_CONTENT
@@ -93,37 +88,37 @@ describe('puppeteer tests', () => {
 	})
 	describe('Navbar Links Test', () => {
 		it('should navigate to homepage', async () => {
-			await loadUrl(page, config.baseUrl)
-			await shouldExist(page, homePage.BANKING_FEATURES)
+			await helper.loadUrl(page, config.baseUrl)
+			await helper.shouldExist(page, homePage.BANKING_FEATURES)
 		})
 
 		it('should have correct number of links', async () => {
-			const numberOfLinks = await getCount(page, '#pages-nav > li')
+			const numberOfLinks = await helper.getCount(page, '#pages-nav > li')
 			expect(numberOfLinks).to.equal(3)
 		})
 	})
 	describe('Feedback Test', () => {
 		it('should navigate to homepage', async () => {
-			await loadUrl(page, config.baseUrl)
-			await shouldExist(page, homePage.BANKING_FEATURES)
+			await helper.loadUrl(page, config.baseUrl)
+			await helper.shouldExist(page, homePage.BANKING_FEATURES)
 		})
 
 		it('should click on feedback link', async () => {
-			await click(page, homePage.LINK_FEEDBACK)
-			await shouldExist(page, feedbackPage.FEEDBACK_FORM)
+			await helper.click(page, homePage.LINK_FEEDBACK)
+			await helper.shouldExist(page, feedbackPage.FEEDBACK_FORM)
 		})
 
 		it('should submit feedback form', async () => {
-			await typeText(page, feedbackPage.FORM_NAME, 'Kaniel')
-			await typeText(page, feedbackPage.FORM_EMAIL, utils.generateEmail())
-			await typeText(page, feedbackPage.FORM_SUBJECT, 'Just Subject')
-			await typeText(page, feedbackPage.FORM_COMMENT, 'Just a comment')
-			await click(page, feedbackPage.FORM_SUBMIT_BUTTON)
+			await helper.typeText(page, feedbackPage.FORM_NAME, 'Kaniel')
+			await helper.typeText(page, feedbackPage.FORM_EMAIL, utils.generateEmail())
+			await helper.typeText(page, feedbackPage.FORM_SUBJECT, 'Just Subject')
+			await helper.typeText(page, feedbackPage.FORM_COMMENT, 'Just a comment')
+			await helper.click(page, feedbackPage.FORM_SUBMIT_BUTTON)
 		})
 
 		it('should display success message', async () => {
-			await shouldExist(page, feedbackResultsPage.FEEDBACK_RESULTS_TITLE)
-			await waitForText(
+			await helper.shouldExist(page, feedbackResultsPage.FEEDBACK_RESULTS_TITLE)
+			await helper.waitForText(
 				page,
 				'Thank you for your comments',
 				feedbackResultsPage.FEEDBACK_RESULTS_CONTENT
@@ -133,29 +128,29 @@ describe('puppeteer tests', () => {
 
 	describe('Forgotten password', async () => {
 		it('should navigate to home page', async () => {
-			await loadUrl(page, config.baseUrl)
-			await shouldExist(page, homePage.BANKING_FEATURES)
+			await helper.loadUrl(page, config.baseUrl)
+			await helper.shouldExist(page, homePage.BANKING_FEATURES)
 		})
 		it('should navigate to login page', async () => {
-			await click(page, homePage.SIGN_IN_BUTTON)
-			await shouldExist(page, loginPage.LOGIN_FORM)
+			await helper.click(page, homePage.SIGN_IN_BUTTON)
+			await helper.shouldExist(page, loginPage.LOGIN_FORM)
 		})
 
 		it('should navigate to forget password page', async () => {
-			await click(page, loginPage.FORGETPASSWORD_LINK)
-			await shouldExist(page, forgetPasswordPage.FGT_PSWD_TITLE)
+			await helper.click(page, loginPage.FORGETPASSWORD_LINK)
+			await helper.shouldExist(page, forgetPasswordPage.FGT_PSWD_TITLE)
 		})
 		it('should submit email', async () => {
-			await typeText(
+			await helper.typeText(
 				page,
 				forgetPasswordPage.EMAIL_FIELD,
 				utils.generateEmail()
 			)
-			await click(page, forgetPasswordPage.SENDPASSWORD_BUTTON)
+			await helper.click(page, forgetPasswordPage.SENDPASSWORD_BUTTON)
 		})
 
 		it('should display success message', async () => {
-			await waitForText(
+			await helper.waitForText(
 				page,
 				'Your password will be sent to the following email',
 				'body'
